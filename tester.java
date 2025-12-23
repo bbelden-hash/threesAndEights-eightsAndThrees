be6965cc616e02d91e626894ceb6e0c9291f187f
Ben Belden <bbelden@uab.edu>
2025-12-23 13:19:54 -0600

    Update tester.java

--- tester.java
+++ tester.java
@@ -3,11 +3,19 @@ public class tester {
 
     public static void main(String[] args) {
 
+        // eightToThree class and function calls
         eightToThree myEncoder = new eightToThree();
         myEncoder.setOctal();
 
         System.out.println("Octal : " + myEncoder.getOctal());
         System.out.println("Binary : " + myEncoder.encoder(myEncoder.getOctal()));
+
+        // threeToEight class and function calls
+        threeToEight myDecode = new threeToEight();
+        myDecode.setBinary();
+        
+        System.out.println("Binary : " + myDecode.getBinary());
+        System.out.println("Octal : " + myDecode.decoder(myDecode.getBinary()));
 
     }
 }
